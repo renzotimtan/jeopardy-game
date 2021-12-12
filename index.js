5f675ea8c72698ecabbf540a85c850ed2396be50
Renzo Tan <renzotimtan@gmail.com>
2021-12-12 19:29:51 +0800

    Score now adds, question interface finished

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const allCategories = document.querySelectorAll(".category");
 const modal = document.getElementById("myModal");
 const categoryIds = [1892, 4483, 88, 218];
 const questionContainer = {};
+let score = 0;
 
 // Fetch the questions
 for (let i = 0; i < categoryIds.length; i++) {
@@ -21,7 +22,7 @@ const generateData = (data, i) => {
     const title = document.querySelector(`.category-${i} p`);
     title.textContent = data.title.toUpperCase();
 
-    // Generate a random value
+    // Generate a random value (to get a random question from filter)
     const q100 = data.clues.filter((clue) => clue.value === 100);
     const rand100 = randomInt(0, q100.length - 1);
 
@@ -47,6 +48,11 @@ const generateData = (data, i) => {
     };
 };
 
+// Tracking question clicked and answer
+let categoryPicked = 0;
+let pointsPicked = 0;
+let correctAnswer = "";
+
 // Use question container
 const changeData = (data, i) => {
     // Get all rows of question cards to be displayed
@@ -63,27 +69,50 @@ const changeData = (data, i) => {
         // If index is 0, it is 100. If index is 1, its is 200, etc.
         const questionObj = category[(q + 1) * 100];
 
-        // Replace text of each box with question and value
-        displayedQuestions[
-            q
-        ].textContent = `${questionObj.question}, [${questionObj.value}]`;
+        // Replace text of each box with value
+        displayedQuestions[q].textContent = questionObj.value;
 
         // Add event listener when clicked
         displayedQuestions[q].addEventListener("click", (e) => {
+            // Open modal
             modal.style.display = "block";
+
+            // reinitialize vars to track what question was picked and answer
+            categoryPicked = data.title;
+            pointsPicked = questionObj.value;
+            correctAnswer = questionObj.answer;
+            console.log(correctAnswer);
+
+            //  Add question text to modal
             document.querySelector(".clicked-question").textContent =
                 questionObj.question;
+
+            // Disable question
+            e.target.style.visibility = "hidden";
+            e.target.style.backgroundColor = "gray";
         });
     }
 };
 
-// Dummy close when answer given (to be changed)
-document.querySelector(".enter-answer").addEventListener("click", () => {
+const resolveAnswer = () => {
+    const answer = document.querySelector(".answer");
+    if (answer.value.toLowerCase().trim() == correctAnswer.toLowerCase()) {
+        score += pointsPicked;
+        document.querySelector(".score").textContent = score;
+    }
+    answer.value = "";
     modal.style.display = "none";
+};
+
+// Resolve answer if enter button is click
+document.querySelector(".enter-answer").addEventListener("click", () => resolveAnswer());
+
+// Resolve answer if enter button key is pressed
+document.querySelector(".answer").addEventListener("keyup", (e) => {
+    if (e.keyCode === 13) resolveAnswer();
 });
 
 // Random number generator
 function randomInt(min, max) {
-    // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
